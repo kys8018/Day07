dec587df24ec9a1d28288f02e396dfea6ad5cff6
lecture <ksw617@gmail.com>
2025-01-21 20:19:55 +0900

    Update cMain.cpp

--- Day07/cMain.cpp
+++ Day07/cMain.cpp
@@ -1,15 +1,54 @@
 #include <stdio.h>
 #include <Windows.h>
 
+#pragma region Enum
+enum Color
+{
+	Black,
+	DarkBlue,
+	DarkGreen,
+	DarkCyan,
+	DarkRed,
+	DarkMagenta,
+	DarkYellow,
+	Gray,
+	DarkGray,
+	Blue,
+	Green,
+	Cyan,
+	Red,
+	Magenta,
+	Yellow,
+	White,
+};
+
+enum SCENE_ID
+{
+	LOGO,
+	MENU,
+	STAGE,
+};
+#pragma endregion
+#pragma region WIN_API
+void HideCursor();
 void SetPosition(int x, int y);
+void ChangeColor(Color color);
+#pragma endregion
+
+
 
 int main()
 {
+	HideCursor();
+
 	while (true)
 	{
 		system("cls");
-		SetPosition(10, 10);
-		printf("hello world");
+		
+		SetPosition(10,10);
+		ChangeColor(Yellow);
+		printf("Hello world");
+
 		Sleep(50);
 
 	}
@@ -17,6 +56,7 @@ int main()
 	return 0;
 }
 
+#pragma region WIN_API
 //커서 이동
 void SetPosition(int x, int y)
 {
@@ -25,3 +65,21 @@ void SetPosition(int x, int y)
 	pos.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+
+//커서 보이는거 방지
+void HideCursor()
+{
+	CONSOLE_CURSOR_INFO info;
+	info.bVisible = false;
+	info.dwSize = 1;
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+}
+
+//색깔 바꾸기
+void ChangeColor(Color color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+#pragma endregion
+
