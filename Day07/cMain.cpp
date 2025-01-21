4a54a41ade7e3a0c89da10cb786164f0031f6920
kys8018 <wsx4362@gmail.com>
2025-01-21 20:22:52 +0900

    Update cMain.cpp

--- Day07/cMain.cpp
+++ Day07/cMain.cpp
@@ -3,16 +3,55 @@
 
 void SetPosition(int x, int y);
 
+struct Obj
+{
+	int x;
+	int y;
+	const char* shape;
+};
+
+//데이터 여기저기 사용가능
+//8byte 정도만 할당
+Obj* player = nullptr; // nullptr == 0
+
 int main()
 {
+	player = (Obj*)malloc(sizeof(Obj));
+	player->x = 10;
+	player->y = 10;
+	player->shape = "옷";
+
 	while (true)
 	{
 		system("cls");
-		SetPosition(10, 10);
-		printf("hello world");
+
+		if (GetAsyncKeyState(VK_LEFT))
+		{
+			player->x--;
+		}
+		if (GetAsyncKeyState(VK_RIGHT))
+		{
+			player->x++;
+		}
+		if (GetAsyncKeyState(VK_UP))
+		{
+			player->y--;
+		}
+		if (GetAsyncKeyState(VK_DOWN))
+		{
+			player->y++;
+		}
+
+		SetPosition(player->x, player->y);
+		printf(player->shape);
+
+
 		Sleep(50);
 
 	}
+
+	free(player);
+	player = nullptr;
 
 	return 0;
 }
@@ -25,3 +64,4 @@ void SetPosition(int x, int y)
 	pos.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+
